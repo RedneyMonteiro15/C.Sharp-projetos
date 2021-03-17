232c0b2cc7cf6f06d94205fb6289b85da4c61bad
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2021-03-17 23:30:29 +0000

    criando projetos

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -6,7 +6,14 @@ namespace calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num;
+            Console.Write("Digite um número: ");
+            num = int.Parse(Console.ReadLine());
+            if(num % 2 == 0){
+                Console.WriteLine($"O número {num} é PAR.");
+            }else{
+                Console.WriteLine($"O número {num} é IMPAR.");
+            }
         }
     }
 }
